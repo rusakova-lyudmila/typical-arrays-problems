935d64645ee04e575aa011a5d6d36de08e604964
Leonid Rusakov <bengunn87@mail.ru>
2020-10-02 19:52:38 +0500

    feat: complete min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array && array.length > 0) ? Math.min(...array) :  0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array && array.length > 0) ? Math.max(...array) :  0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (array && array.length > 0) 
+    ? array.reduce((sum, item) => sum + item) / array.length
+    : 0
+  ;    
 }
